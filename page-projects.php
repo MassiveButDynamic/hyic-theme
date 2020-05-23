b9f39c1ee6fdea9cac2c855ad5745fb2b60d0306
Moritz <moritz.ahrens2@gmail.com>
2020-05-24 00:46:15 +0200

    Changed projects layout

--- page-projects.php
+++ page-projects.php
@@ -18,14 +18,13 @@ $loop = new WP_Query($args);
     while ( $loop->have_posts() ) {
         $loop->the_post();
         ?>
-        <a href='<?php the_permalink();?>'>
-        <div class='project'>
+        <a class='project' href='<?php the_permalink();?>'>
+        
             <div class='image'><?php the_post_thumbnail(); ?></div>
             <div class='text'>
                 <h2><?php the_title(); ?></h2>
                 <div class='description'><?php the_content(); ?></div>
             </div>
-        </div>
         </a>
         <?php
     }

c805a3facbcbc6046a83477994db0183e9474984
Moritz <moritz.ahrens2@gmail.com>
2020-05-24 14:37:38 +0200

    Implemented custom tile colors for projects

--- page-projects.php
+++ page-projects.php
@@ -21,7 +21,7 @@ $loop = new WP_Query($args);
         <a class='project' href='<?php the_permalink();?>'>
         
             <div class='image'><?php the_post_thumbnail(); ?></div>
-            <div class='text'>
+            <div class='text' style='background-color: <?php echo get_post_custom_values('_hyic_project_tile_color', get_the_ID())[0];?>'>
                 <h2><?php the_title(); ?></h2>
                 <div class='description'><?php the_content(); ?></div>
             </div>

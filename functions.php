c2bf3a6e559e3822f2e0a101fa1c3b3075e73d23
Moritz <moritz.ahrens2@gmail.com>
2020-12-19 23:34:56 +0100

    Basic styling for hyic-events-carousel

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@
     add_theme_support('customize-selective-refresh-widgets');
     add_theme_support('post-thumbnails');
 
-    //wp_enqueue_style('projects', get_template_directory_uri().'/css/projects.css');
+    wp_enqueue_style('hyic-event-carousel', get_template_directory_uri().'/css/hyic-event-carousel.css');
     //wp_enqueue_style('customizer', get_template_directory_uri().'/css/customizer.css');
 
     //wp_enqueue_script('jquery');

9ee8f9738bdf95e9a537d5952c53e0a7886b1ad1
Moritz <moritz.ahrens2@gmail.com>
2021-01-20 12:32:42 +0100

    Optimized for a better lighthouse score

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
-<html>
+<!DOCTYPE html>
+<html lang='de-DE'>
     <head>
         <title><?php bloginfo('name') ?></title>
         <meta name='description' content='<?php bloginfo('description') ?>'>
@@ -8,9 +9,9 @@
     </head>
     <body>
         <nav class='<?php if(is_front_page()) { echo('show-on-scroll'); }?>'>
-            <a href='<?php echo get_home_url(); ?>'><img class='logo' src='<?php echo esc_url(get_theme_mod('menu_logo')); ?>'></a>
+            <a href='<?php echo get_home_url(); ?>'><img class='logo' src='<?php echo esc_url(get_theme_mod('menu_logo'));?>' alt='Das Logo des HYIC'></a>
             <?php wp_nav_menu(array('menu'=>'top')) ?>
-            <div class='hamburger' onclick='toggleMobileNav()'><img src='<?php echo get_template_directory_uri(); ?>/assets/menu-24px.svg'></div>
+            <div class='hamburger' onclick='toggleMobileNav()'><img src='<?php echo get_template_directory_uri(); ?>/assets/menu-24px.svg' alt='Hamburger-Menü'></div>
         </nav>
         <div id='mobilenav-wrapper'>
             <?php wp_nav_menu(array('menu'=>'top')) ?>

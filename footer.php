6f94e91bb10633fe5ae0c36d0a186328b0f5d226
Moritz <moritz.ahrens2@gmail.com>
2021-04-12 23:07:26 +0200

    Call wp_footer in footer

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
             <nav class='footer-nav'>
             <?php wp_nav_menu(array('menu'=>'footer')) ?>
             </nav>
+        <?php wp_footer(); ?>
         </footer>
     </body>
 </html>
